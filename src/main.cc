71091f9c374dc285d4c75e04380abeda09f3ae10
emzilia <em@zilia.dev>
2026-05-29 23:51:27 -0400

    prints list of all dirs within ~/repos

---
+++ src/main.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <filesystem>
+#include <git2.h>
+
+std::vector<std::string> repo_dirs;
+std::vector<std::string>::iterator ri;
+
+std::vector<std::string> get_repos() {
+	std::string home_path = getenv("HOME");
+	std::string repo_path = home_path + "/repos/";
+
+	for (const auto & entry : std::filesystem::directory_iterator(repo_path)) {
+		if (entry.is_directory()) {
+			repo_dirs.push_back(entry.path());
+		}
+	}
+
+	return repo_dirs;
+}
+
+int main() {
+	git_libgit2_init();
+
+	repo_dirs = get_repos();
+
+	for (ri = repo_dirs.begin(); ri != repo_dirs.end(); ++ri) {
+		std::cout << *ri << std::endl;
+	}
+	
+	git_libgit2_shutdown();
+}
